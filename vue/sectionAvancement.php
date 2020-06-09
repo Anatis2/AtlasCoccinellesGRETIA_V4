01e499479b228dc5e5495261acec2bfb62bc61d3
Anatis2 <claire.coubard@gmail.com>
2020-06-09 11:55:13 +0200

    Mise en ligne des cartes à l'espèce

--- vue/sectionAvancement.php
+++ vue/sectionAvancement.php
@@ -90,51 +90,97 @@
                     A venir...
                 </p>
             </div>
-
-            <div class="margeDiv">
-                <a href="#" class="retourHautDePage">Retour en haut de page</a>
-            </div>
         </section>
 
-        <section class="container" id="sectionAvancementCartesEspeces">
+
+        <section id="sectionAvancementCartesEspeces">
             <h3>Avancement de l'atlas (cartes à l'espèce)</h3>
 
-                <h4>Lambert93</h4>
+                <div class="accordion" id="accordionExample">
+                    <button class="btn btn-link text-left dropdown-toggle" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Maillage Lambert93
+                    </button>
+                    <br/><br/>
 
-                <div class="row">
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                        
+                        <div class="row">
 
-                <?php
-                    $ext_list = Array("jpg", "jpeg", "bmp", "gif", "png"); // Liste des extensions de photo 
-                    $listephotos = Array(); 
-                    $dossier = opendir("./images/Cartes/GrandOuest2020L93/"); // Ouvre le dossier 
-                    for($i=0 ; $f=readdir($dossier) ; $i++) { 
-                        $listephotos[$i] = $f; // Ajoute la photo  
-                    }
-                    closedir($dossier); // On n'a plus besoin du dossier 
-                    sort($listephotos); // Trie par ordre alphabétique 
-                    // Et maintenant, on affiche. 
-                    foreach($listephotos as $nom) { 
-                        $nbASuppr = 4;
-                        $nomSansExtension = substr($nom, 0, -$nbASuppr);
-                        if ($nom != "." && $nom != "..") {
-                ?>
+                        <?php
+                            $ext_list = Array("jpg", "jpeg", "bmp", "gif", "png"); // Liste des extensions de photo 
+                            $listephotos = Array(); 
+                            $dossier = opendir("./images/Cartes/GrandOuest2020L93/"); // Ouvre le dossier 
+                            for($i=0 ; $f=readdir($dossier) ; $i++) { 
+                                $listephotos[$i] = $f; // Ajoute la photo  
+                            }
+                            closedir($dossier); // On n'a plus besoin du dossier 
+                            sort($listephotos); // Trie par ordre alphabétique 
+                            // Et maintenant, on affiche. 
+                            foreach($listephotos as $nom) { 
+                                $nbASuppr = 4;
+                                $nomSansExtension = substr($nom, 0, -$nbASuppr);
+                                if ($nom != "." && $nom != "..") {
+                        ?>
 
-                <div class="col" style="margin-bottom: 60px;">
-                    <a href="./images/Cartes/GrandOuest2020L93/<?php echo $nom; ?>" target="_blank">
-                        <img src="./images/Cartes/GrandOuest2020L93/<?php echo $nom; ?>" style="min-width: 300px; width: 60%;" class="imageReduiteCol" alt="<?php echo $nom; ?>"/>
-                    </a>
-                    <figcaption><strong><?php echo $nomSansExtension; ?></strong></figcaption>
-                </div>
+                        <div class="col" style="margin-bottom: 60px; text-align: center;">
+                            <a href="./images/Cartes/GrandOuest2020L93/<?php echo $nom; ?>" target="_blank">
+                                <img src="./images/Cartes/GrandOuest2020L93/<?php echo $nom; ?>" style="min-width: 300px; width: 60%;" class="imageReduiteCol" alt="<?php echo $nom; ?>"/>
+                            </a>
+                            <p style="margin-top: 10px;"><strong><?php echo $nomSansExtension; ?></strong></p>
+                        </div>
 
 
-                <?php
-                        }
-                    }
-                ?>
+                        <?php
+                                } elseif ($nom = "") {
+                                    echo "Vous n'avez pas accès à cette page";
+                                }
+                            }
+                        ?>
 
-                </div>
+                        </div>
+                    </div>
 
-                <div class="margeDiv">
-                    <a href="#" class="retourHautDePage">Retour en haut de page</a>
+                    <button class="btn btn-link text-left dropdown-toggle" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">
+                        Maillage UTM
+                    </button>
+                    <br/><br/>
+
+                    <div id="collapseTwo" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                        
+                        <div class="row">
+
+                        <?php
+                            $ext_list = Array("jpg", "jpeg", "bmp", "gif", "png"); // Liste des extensions de photo 
+                            $listephotos = Array(); 
+                            $dossier = opendir("./images/Cartes/GrandOuest2020UTM/"); // Ouvre le dossier 
+                            for($i=0 ; $f=readdir($dossier) ; $i++) { 
+                                $listephotos[$i] = $f; // Ajoute la photo  
+                            }
+                            closedir($dossier); // On n'a plus besoin du dossier 
+                            sort($listephotos); // Trie par ordre alphabétique 
+                            // Et maintenant, on affiche. 
+                            foreach($listephotos as $nom) { 
+                                $nbASuppr = 4;
+                                $nomSansExtension = substr($nom, 0, -$nbASuppr);
+                                if ($nom != "." && $nom != "..") {
+                        ?>
+
+                        <div class="col" style="margin-bottom: 60px; text-align: center;">
+                            <a href="./images/Cartes/GrandOuest2020UTM/<?php echo $nom; ?>" target="_blank">
+                                <img src="./images/Cartes/GrandOuest2020UTM/<?php echo $nom; ?>" style="min-width: 300px; width: 60%;" class="imageReduiteCol" alt="<?php echo $nom; ?>"/>
+                            </a>
+                            <p style="margin-top: 10px;"><strong><?php echo $nomSansExtension; ?></strong></p>
+                        </div>
+
+
+                        <?php
+                                }
+                            }
+                        ?>
+
+                        </div>
+
+                    </div>
+                    
                 </div>
         </section>
